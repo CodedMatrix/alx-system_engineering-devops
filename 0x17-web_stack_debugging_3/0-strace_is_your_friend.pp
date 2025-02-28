b0c3d96a04c68c5e63379741de5eb18b67dec5c2
CodedMatrix <umuhozaallykhaled@gmail.com>
2025-02-28 10:48:15 +0200

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,8 +1,7 @@
 # 0-strace_is_your_friend.pp
-# Puppet manifest to fix Apache 500 error by correcting file extension typo
+# Puppet manifest to fix Apache 500 error
 
 exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  command => 'sed -i s/class-wp-locale.phpp/class-wp-locale.php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin:/usr/bin:/bin',
-  onlyif  => 'grep -q phpp /var/www/html/wp-settings.php',
 }
